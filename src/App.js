1de9c63d9d735ecb155b9094343ac7ccf4551cda
soodang <ehdgodgka@gmail.com>
2020-07-31 23:26:56 +0900

    created shop page : collection-preview ,collection-item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-const TopicPage = () => (
-  <div>
-    <h1>TOPIC PAGE </h1>
-    <TopicList />
-  </div>
-);
-const TopicList = () => <div>Topic List</div>;
 function App() {
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={TopicPage} />
-        <Route path='/jackets' component={TopicPage} />
-        <Route path='/sneakers' component={TopicPage} />
-        <Route path='/womens' component={TopicPage} />
-        <Route path='/mens' component={TopicPage} />
+        <Route path='/shop' component={ShopPage} />
         <Redirect to='/' />
       </Switch>
     </div>

7cae6a42b78cd0ca0074c515a7e6b498f5b47060
soodang <ehdgodgka@gmail.com>
2020-07-31 18:07:11 +0900

    directory menu router setting with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
+const TopicPage = () => (
+  <div>
+    <h1>TOPIC PAGE </h1>
+    <TopicList />
+  </div>
+);
+const TopicList = () => <div>Topic List</div>;
 function App() {
   return (
     <div className='App'>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={TopicPage} />
+        <Route path='/jackets' component={TopicPage} />
+        <Route path='/sneakers' component={TopicPage} />
+        <Route path='/womens' component={TopicPage} />
+        <Route path='/mens' component={TopicPage} />
+        <Redirect to='/' />
+      </Switch>
     </div>
   );
 }
